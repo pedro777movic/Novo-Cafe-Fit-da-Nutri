5a350ca5ca8eb3c0e8e458372ecea3c8a5b515f8
Pedro Barbosa Barbosa <pedrobarbosa2971@gmail.com>
2026-02-13 22:25:30 +0000

    Nada para resistir (_for element span_)

--- src/components/landing/experience.tsx
+++ src/components/landing/experience.tsx
@@ -8,11 +8,11 @@ const features = [
   },
   {
     icon: <CheckCircle className="w-8 h-8 text-primary" />,
-    description: "Nada para calcular",
+    description: "Nada para resistir",
   },
   {
     icon: <CheckCircle className="w-8 h-8 text-primary" />,
-    description: "Nada para resistir",
+    description: "Nada para calcular",
   },
 ]
 
